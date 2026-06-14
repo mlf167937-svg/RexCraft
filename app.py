7d9acd02edf5673f1ec7115452d532459f97156f
mlf167937-svg <mlf167937@gmail.com>
2026-06-14 08:30:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
+def read_txt(filename):
+    path = os.path.join("static", filename)
+    try:
+        with open(path, "r", encoding="utf-8") as f:
+            return f.read()
+    except:
+        return "Teks belum tersedia."
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    data = [
+        {
+            "img": "history1.png",
+            "text": read_txt("history1.txt")
+        },
+        {
+            "img": "history2.jpg",
+            "text": read_txt("history2.txt")
+        }
+    ]
+    return render_template("index.html", data=data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
